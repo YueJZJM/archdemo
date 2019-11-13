d85fb85b2c0654caf9a5fb69c67044dceda47ca8
岳剑钟 <yuejzhfut@163.com>
2019-11-14 01:08:10 +0800

    mainScope

--- app/src/main/java/com/yuejianzhong/archdemo/coroutines/WxarticleActivity.kt
+++ app/src/main/java/com/yuejianzhong/archdemo/coroutines/WxarticleActivity.kt
@@ -4,23 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.yuejianzhong.archdemo.R
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class WxarticleActivity : AppCompatActivity() {
+
+    val mainScope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wxarticle)
 
-        GlobalScope.launch(Dispatchers.Main) {
+        val job = GlobalScope.launch(Dispatchers.Main) {
             withContext(Dispatchers.IO){
                 val dataBean = RetrofitClient.reqApi.getDatas().await()
-                Log.d("yuejz",dataBean.toString())
+//                Log.d("yuejz",dataBean.toString())
             }
             //更新ui
         }
+
+        mainScope.launch {
+            val dataBean = RetrofitClient.reqApi.getDatas().await()
+            Log.d("yuejz",dataBean.toString())
+        }
+//
+//        GlobalScope.launch(Dispatchers.Main +CoroutineExceptionHandler { coroutineContext, throwable ->
+//            print(throwable)
+//        }) {
+////            throw NullPointerException()
+//        }
+//        job.cancel()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+//        job
+        mainScope.cancel()
     }
 }
